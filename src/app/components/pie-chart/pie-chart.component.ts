ebf6c952333e28d7dabaaa04641627d2abdf05ee
EngMeow <mohamed.basyoni02@gmail.com>
2024-01-07 02:55:31 +0200

    almostDone

--- src/app/components/pie-chart/pie-chart.component.ts
+++ src/app/components/pie-chart/pie-chart.component.ts
@@ -29,7 +29,7 @@ export class PieChartComponent {
 
   constructor() {
     this.chartOptions = {
-      series: [250],
+      series: [70],
       chart: {
         height: 110,
         type: "radialBar",
@@ -39,17 +39,14 @@ export class PieChartComponent {
       },
       plotOptions: {
         radialBar: {
-          startAngle: -90,
-          endAngle: 135,
-
-        
+          startAngle: -135,
+          endAngle: 135,      
           hollow: {
             margin: 0,
             size: "70%",
             background: "#fff",
             image: undefined,
             position: "front",
-            
             // dropShadow: {
             //   enabled: true,
             //   top: 3,
@@ -59,16 +56,15 @@ export class PieChartComponent {
             // }
           },
           track: {
-            background: "#fff",
-            strokeWidth: "120px",
+            background: "#E6ECF5",
+            strokeWidth: "100px",
             margin: 0, // margin is in pixels
-            // dropShadow: {
-            //   enabled: true,
-            //   top: -3,
-            //   left: 0,
-            //   blur: 4,
-            //   opacity: 0.35
-            // }
+            dropShadow: {
+              top: -3,
+              left: 0,
+              blur: 2,
+              opacity: 0.35
+            }
           },
 
           dataLabels: {
@@ -95,30 +91,149 @@ export class PieChartComponent {
           }
         }
       },
-      
       fill: {
         type: "gradient",
         gradient: {
-          shade: "dark",
+          shade: "light",
           type: "horizontal",
-          shadeIntensity: 0,
+          shadeIntensity: 0.9,
           gradientToColors: ["#64B0FF"],
           inverseColors: true,
           opacityFrom: 1,
           opacityTo: 1,
-          stops: [0,50, 100]
+          stops: [0, 50, 53, 91]
         }
       },
       stroke: {
         show: true,
         curve: 'stepline',
         lineCap: 'round',
-        colors: ['red'],
-        width: 1,
-        dashArray: 0, 
     },    
 
       labels: ["خدمة"]
     };
   }
 }
+
+
+// import { Component, ViewChild } from "@angular/core";
+// import { ChartComponent } from "ng-apexcharts";
+
+// import {
+//   ApexNonAxisChartSeries,
+//   ApexResponsive,
+//   ApexChart
+// } from "ng-apexcharts";
+
+// export type ChartOptions = {
+//   series: ApexNonAxisChartSeries;
+//   chart: ApexChart;
+//   responsive: ApexResponsive[];
+//   labels: any;
+// };
+
+// @Component({
+//   selector: "app-pie-chart",
+//   templateUrl: "./pie-chart.component.html",
+//   styleUrls: ["./pie-chart.component.css"]
+// })
+// export class PieChartComponent {
+//   @ViewChild("chart") chart: ChartComponent;
+//   public chartOptions: Partial<ChartOptions>;
+
+//   constructor() {
+//     this.chartOptions = {
+//       series: [10, 20, 30],
+//       chart: {
+//         type: "donut"
+//       },
+//       labels: ["Team A", "Team B", "Team C"],
+//       responsive: [
+//         {
+//           breakpoint: 480,
+//           options: {
+//             plotOptions: {
+//               pie: {
+//                 startAngle: 45,
+//                 endAngle: 360,
+//                 expandOnClick: false,
+//                 offsetX: 0,
+//                 offsetY: 0,
+//                 customScale: 1,
+//                 dataLabels: {
+//                     offset: 0,
+//                     minAngleToShowLabel: 3
+
+//                 }, 
+//                 markers: {
+//                   size: 0,
+//                   colors: undefined,
+//                   strokeColors: '#000',
+//                   strokeWidth: 2,
+//                   strokeOpacity: 0.9,
+//                   strokeDashArray: 0,
+//                   fillOpacity: 1,
+//                   discrete: [],
+//                   shape: "circle",
+//                   radius: 2,
+//                   offsetX: 0,
+//                   offsetY: 0,
+//                   onClick: undefined,
+//                   onDblClick: undefined,
+//                   showNullDataPoints: true,
+//                   hover: {
+//                     size: undefined,
+//                     sizeOffset: 3
+//                   }
+//               },
+//                 donut: {
+//                   size: '10%',
+//                   background: 'transparent',
+//                   labels: {
+//                     show: true,
+//                     name: {
+//                       show: true,
+//                       fontSize: '22px',
+//                       fontFamily: 'Helvetica, Arial, sans-serif',
+//                       fontWeight: 600,
+//                       color: undefined,
+//                       offsetY: -10,
+//                       formatter: function (val) {
+//                         return val
+//                       }
+//                     },
+//                     value: {
+//                       show: true,
+//                       fontSize: '16px',
+//                       fontFamily: 'Helvetica, Arial, sans-serif',
+//                       fontWeight: 400,
+//                       color: undefined,
+//                       offsetY: 16,
+//                       formatter: function (val) {
+//                         return val
+//                       }
+//                     },
+//                     total: {
+//                       show: false,
+//                       showAlways: false,
+//                       label: 'Total',
+//                       fontSize: '22px',
+//                       fontFamily: 'Helvetica, Arial, sans-serif',
+//                       fontWeight: 600,
+//                       color: '#373d3f',
+//                       formatter: function (w) {
+//                         return w.globals.seriesTotals.reduce((a, b) => {
+//                           return a + b
+//                         }, 0)
+//                       }
+//                     }
+//                   }
+//                 },      
+//               }
+//             }
+//         }
+//       }
+//       ]
+//     };
+//   }
+// }
--- src/app/components/products/products.component.ts
+++ src/app/components/products/products.component.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import { Iproduct } from 'src/app/models/i-product';
+import { Iproduct , ProductStatus} from 'src/app/models/i-product';
 import { productsList } from 'src/app/models/product-list';
 
 @Component({
@@ -9,4 +9,31 @@ import { productsList } from 'src/app/models/product-list';
 })
 export class ProductsComponent {
   products: Iproduct[] = productsList;
+  ProductStatus = ProductStatus;
+
+  getBackgroundColor(productStatus: ProductStatus): string {
+    switch (productStatus) {
+      case ProductStatus.Available:
+        return 'rgba(39, 208, 140, 0.10)'; // Set your desired color for Available status
+      case ProductStatus.Unavailable:
+        return '#FEEEEE'; // Set your desired color for Unavailable status
+      case ProductStatus.AlmostFinish:
+        return '#FEF8E6'; // Set your desired color for AlmostFinish status
+      default:
+        return 'transparent'; // Set a default color if needed
+    }
+  }
+
+  getFontColor(productStatus: ProductStatus): string {
+    switch(productStatus) {
+      case ProductStatus.Available:
+        return 'bg-success'; // Set your desired color for Available status
+      case ProductStatus.Unavailable:
+        return 'bg-danger'; // Set your desired color for Unavailable status
+      case ProductStatus.AlmostFinish:
+        return 'bg-warning'; // Set your desired color for AlmostFinish status
+      default:
+        return 'transparent'; // Set a default color if needed
+    }
+   }
 }
